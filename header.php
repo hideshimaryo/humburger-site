cf2f3d5fda4d132e8813937051bea4655a78db7c
hideshimaryo <ryoh56762@gmail.com>
2023-07-02 21:55:03 +0900

    管理バー修正

--- header.php
+++ header.php
@@ -6,6 +6,13 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php bloginfo('name'); ?></title>
+  <?php if( is_user_logged_in() ) : ?>
+<style type="text/css">
+html.html {
+  margin-top: 32px;
+}
+</style>
+<?php endif; ?>
   <?php wp_head(); ?>
 </head>
 
