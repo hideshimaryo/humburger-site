85cdda486a719d2f7731dff70b779e26a2f90611
hideshimaryo <ryoh56762@gmail.com>
2023-07-04 22:55:16 +0900

    サイドバー修正

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ $(function(){
     $('.l-sidebar').toggleClass('is-open');
     $('.p-hamburger__mask').toggleClass('is-open');
     // $('body').toggleClass('is-open');
+    $('.l-main-contents').toggleClass('is-open');
     $('.l-sidebar__menu-list').toggleClass('is-open');
   });
 
@@ -12,12 +13,15 @@ $(function(){
   $('.p-hamburger__mask').on('click', function() {
     $('.l-sidebar').toggleClass('is-open');
     $('.p-hamburger__mask').toggleClass('is-open');
+    $('.l-main-contents').toggleClass('is-open');
   });
 
   // リンクをクリックした時にメニューを閉じる
   $('.l-sidebar__close').on('click', function() {
-    $('.l-sidebar').removeClass('is-open');
-    $('.p-hamburger__mask').removeClass('is-open');
+    $('.l-sidebar').toggleClass('is-open');
+    $('.p-hamburger__mask').toggleClass('is-open');
+    $('.l-sidebar__menu-list').toggleClass('is-open');
+    $('.l-main-contents').toggleClass('is-open');
   });
 });
 });
